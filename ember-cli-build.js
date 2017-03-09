bfdbb4f2cb564bf7e2def008f7313d3b6a8129e8
Ryan Mark <rgmark@gmail.com>
2017-03-09 10:18:28 -0800

    update to latest ember-cli

---
+++ ember-cli-build.js
@@ -0,0 +1,24 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function(defaults) {
+  var app = new EmberAddon(defaults, {
+    // Add options here
+  });
+
+  /*
+    This build file specifies the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+  app.import(app.bowerDirectory + '/ember-cli-snap-svg-shim/snap-svg-shim.js', {
+    exports: {
+      'mina': ['default'],
+      'snap-svg': ['default']
+    }
+  });
+
+  return app.toTree();
+};
